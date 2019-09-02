516caf772605254926934cd79dce8ebdeb2641af
duanyuanping <1798482104@qq.com>
2019-09-02 09:50:57 +0800

    feat: init

---
+++ components/Test/index.tsx
@@ -0,0 +1,3 @@
+import './index.less';
+
+export default () => <div>Welcdddddddddddddddddddome to next.js!</div>
---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+/** 
+ * @date 2019/9/2
+ * 入口
+*/
+
+import Test from '../components/Test'
+
+export default () => <Test />
